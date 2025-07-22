79096908bf4e89f53deaf070756ee3cbf98c6f40
lenonmerlo <lenontm@gmail.com>
2025-07-22 17:50:48 -0300

    refactor: extrai botão principal para componente FokusButton

--- app/index.jsx
+++ app/index.jsx
@@ -1,62 +1,60 @@
 import { useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { FokusButton } from "../components/FokusButton";
 
 const pomodoro = [
   {
-    id: 'focus',
+    id: "focus",
     initialValue: 25,
     image: require("./pomodoro.png"),
-    display: "Foco"
+    display: "Foco",
   },
   {
-    id: 'short',
+    id: "short",
     initialValue: 5,
     image: require("./short.png"),
-    display: "Pausa curta "
+    display: "Pausa curta ",
   },
   {
-    id: 'long',
+    id: "long",
     initialValue: 15,
     image: require("./long.png"),
-    display: "Pausa longa "
-  }
-]
+    display: "Pausa longa ",
+  },
+];
 
 export default function Index() {
-
-  const [timerType, setTimerType] = useState(pomodoro[0])
+  const [timerType, setTimerType] = useState(pomodoro[0]);
 
   return (
-    <View
-      style={styles.container}
-    >
-      <Image source={timerType.image}/>
+    <View style={styles.container}>
+      <Image source={timerType.image} />
       <View style={styles.actions}>
         <View style={styles.context}>
-          {pomodoro.map(p => (
-          <Pressable
-            key={p.id}
-            style={ timerType.id === p.id ? styles.contextButtonActive : null }
-            onPress={() => setTimerType(p)}
-          >
-            <Text style={styles.contextButtonText}>
-              {p.display}
-            </Text>
-          </Pressable>
+          {pomodoro.map((p) => (
+            <Pressable
+              key={p.id}
+              style={timerType.id === p.id ? styles.contextButtonActive : null}
+              onPress={() => setTimerType(p)}
+            >
+              <Text style={styles.contextButtonText}>{p.display}</Text>
+            </Pressable>
           ))}
         </View>
         <Text style={styles.timer}>
-          { new Date(timerType.initialValue * 1000).toLocaleTimeString("pt-BR", { minute: "2-digit", second: "2-digit"}) }
+          {new Date(timerType.initialValue * 1000).toLocaleTimeString("pt-BR", {
+            minute: "2-digit",
+            second: "2-digit",
+          })}
         </Text>
-        <Pressable style={styles.button}>
-          <Text style={styles.buttonText}>Começar</Text>
-        </Pressable>
+        <FokusButton />
       </View>
       <View style={styles.footer}>
-        <Text style={styles.footerText}>Projeto de estudo feito por Lenon Merlo</Text>
-      </View>    
+        <Text style={styles.footerText}>
+          Projeto de estudo feito por Lenon Merlo
+        </Text>
+      </View>
     </View>
-    
   );
 }
 
@@ -66,7 +64,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#021123",
-    gap: 40
+    gap: 40,
   },
   actions: {
     padding: 24,
@@ -75,40 +73,30 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     width: "80%",
     borderColor: "#144480",
-    gap: 32
+    gap: 32,
   },
   context: {
     flexDirection: "row",
     justifyContent: "space-around",
-    alignItems: "center"
+    alignItems: "center",
   },
   contextButtonActive: {
     backgroundColor: "#144480",
-    borderRadius: 8
+    borderRadius: 8,
   },
   contextButtonText: {
     fontSize: 12.5,
     color: "#FFF",
-    padding: 8
+    padding: 8,
   },
   timer: {
     fontSize: 54,
     fontWeight: "bold",
     color: "#FFF",
-    textAlign: "center"
-  },
-  button: {
-    backgroundColor: "#B872FF",
-    borderRadius: 32,
-    padding: 8,
-  },
-  buttonText: {
     textAlign: "center",
-    color: "#021123",
-    fontSize: 18
   },
   footer: {
-    width: "80%"
+    width: "80%",
   },
 
   footerText: {
@@ -116,5 +104,5 @@ const styles = StyleSheet.create({
     color: "#98a0a8",
     fontSize: 12.5,
     marginBottom: 16,
-  }
-})
+  },
+});
