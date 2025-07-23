90ace32dc24565bf2ccce2bd0d9f612c78f939aa
lenonmerlo <lenontm@gmail.com>
2025-07-23 09:36:57 -0300

    refactor: extrai componente Timer para exibição do tempo formatado

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { FokusButton } from "../components/FokusButton";
 import { ActionButton } from "../components/ActionButton";
+import { Timer } from "../components/Timer";
 
 const pomodoro = [
   {
@@ -41,12 +42,7 @@ export default function Index() {
             />
           ))}
         </View>
-        <Text style={styles.timer}>
-          {new Date(timerType.initialValue * 1000).toLocaleTimeString("pt-BR", {
-            minute: "2-digit",
-            second: "2-digit",
-          })}
-        </Text>
+        <Timer totalSeconds={timerType.initialValue}/>
         <FokusButton />
       </View>
       <View style={styles.footer}>
@@ -79,12 +75,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
-  },
-  timer: {
-    fontSize: 54,
-    fontWeight: "bold",
-    color: "#FFF",
-    textAlign: "center",
   },
   footer: {
     width: "80%",
