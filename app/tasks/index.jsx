2feb14b3ff45a29d2006f7aadde1a492e9cee78e
lenonmerlo <lenontm@gmail.com>
2025-07-24 11:20:55 -0300

    feat: aplica estilo visual no TaskItem com ícones SVG e ajuste na prop 'checked'

--- app/tasks/index.jsx
+++ app/tasks/index.jsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import TaskItem from "../../components/TaskItem";
 
 export default function Tasks() {
   return (
     <View>
       <Text>Página para listar tarefas</Text>
+      <TaskItem completed text={"Estudar React"} />
+      <TaskItem text={"Estudar React Native"} />
     </View>
   );
 }
